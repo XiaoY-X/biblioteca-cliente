2da9b00d0b4bcd8867c134f669bb5233272f99f1
Clemente-Alvarez <ivancho.alvarez@gmail.com>
2025-05-05 18:21:20 +0200

    added tests

--- cliente/src/main/java/sos/biblioteca/cliente/Cliente.java
+++ cliente/src/main/java/sos/biblioteca/cliente/Cliente.java
@@ -36,17 +36,17 @@ public class Cliente {
 		service.postEjemplar( 1, "disponible");
 		service.postEjemplar( 2, "disponible");
 
-		service.postPrestamo(1,"2-05-2025")
+		service.postPrestamo(1,"2-05-2025");
 
 
 		/// libros GET, POST 
 
 		System.out.println(" ### Obtener listado libros ### ");
-		service.getLibros();; 
+		service.getLibros(); 
 		System.out.println();
 
 		System.out.println(" ### Obtener listado libros paginado ### ");
-		service.getLibros(0,3,"","");; 
+		service.getLibros(0,3,"",""); 
 		System.out.println();
 
 
@@ -58,39 +58,80 @@ public class Cliente {
 
 		// /libros/{libro-id} - PUT, DELETE 
 
+		System.out.println(" ### Modificar libro ### ");
+		service.putLibro(0, "9788437640099", "20000 de viaje submarino", "Julio Verne", "ES 16/05/2019", "Editorial Catedra");
+		service.putLibro(1, "9788437640099", "20000 de viaje submarino", "Julio Verne", "ES 16/05/2019", "Editorial Catedra");
+		service.putLibro(1, "9788420674179", "El señor de las moscas", "William Golding", "ES 13/05/2010", "Alianza Editorial");
+		System.out.println();
 
-
-		// /libros/{libro-id}/ejemplares -  POST 
-
-
-		// /libros/{libro-id}/ejemplares/{ejemplar-id} - DELETE 
-
-
-
-
-		// /usuarios - GET, POST 
-
-		System.out.println(" ### Obtener listado libros ### ");
-		service.getUsuarios();
+		System.out.println(" ### Modificar libro ### ");
+		service.deleteLibro(0);
 		System.out.println();
 
 
 
+		// /libros/{libro-id}/ejemplares -  POST 
 
+		System.out.println(" ### Añadir ejemplar ### ");
+		service.postEjemplar(1, "disponible");
+		service.postEjemplar(1, "disponible");
+		service.postEjemplar(0, "disponible");
+		System.out.println();
+
+
+		// /libros/{libro-id}/ejemplares/{ejemplar-id} - DELETE 
+
+		System.out.println(" ### eliminar ejemplar ### ");
+		service.deleteEjemplar(1, 0);
+
+
+		// /usuarios - GET, POST 
+
+		System.out.println(" ### Obtener listado Usuarios ### ");
+		service.getUsuarios();
+		System.out.println();
+
+		System.out.println(" ### Añadir Usuario ### ");
+		service.postUsuario("Miguel", "01/01/2020", "miguel@mail.com", null);
+		service.postUsuario("Pablo", "13/02/2004", "pablo@mail.com", "4/5/2025");
+		service.postUsuario("Ana", "23/03/2000", "ana@mail.com", "");
+		System.out.println();
 
 		// /usuarios/{matricula} - GET, PUT, DELETE 
 
+		System.out.println(" ### Obtener Usuario ### ");
+		service.getUsuario(0);
+		System.out.println();
 
+		System.out.println(" ### Obtener Usuario ### ");
+		service.putUsuario(1, "Iván", "06/03/2004", "ivan.alvarez@mail.com", "05/05/2025");
+		service.putUsuario(0, "Pablo", "13/02/2004", "pablo@mail.com", "4/5/2025");
+		service.putUsuario(0, "Iván", "06/03/2004", "ivan.alvarez@mail.com", "05/05/2025");
+		System.out.println();
 
+		System.out.println(" ### Eliminar Usuario ### ");
+		service.deleteUsuario(1);
+		System.out.println();
 
 		// /usuarios/{matricula}/prestamos  - GET, POST 
 
+		System.out.println(" ### Obtener Prestamos ### ");
+		service.getPrestamos(0);
+		System.out.println();
 
-
+		System.out.println(" ### Añadir Prestamo ### ");
+		service.postPrestamo(0, "05/05/2025");
+		service.postPrestamo(1, "01/05/2025");
+		service.postPrestamo(0, "05/05/2025");
+		System.out.println();
 
 		// /usuarios/{matricula}/prestamos/{pretamo-id} -PUT 
 
-
+		System.out.println(" ### Obtener Prestamo ### ");
+		service.putPrestamo(0, 1, "05/05/2025", "12/05/2025", false);
+		service.putPrestamo(1, 0, "01/05/2025", "6/05/2025", false);
+		service.putPrestamo(0, 0, "05/05/2025", "12/05/2025", true);
+		System.out.println();
 
 		System.out.println("Fin de las pruebas");
 
